0edf06a9ccb0d29478d1ce23cfd7d3423ed67e1f
DYShunyaev <shunyaevd@gmail.com>
2025-09-14 14:57:30 +0300

    Добавил все остальное

---
+++ src/main/java/d/shunyaev/RemoteTrainingApp/RemoteTrainingAppApplication.java
@@ -0,0 +1,13 @@
+package d.shunyaev.RemoteTrainingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTrainingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteTrainingAppApplication.class, args);
+	}
+
+}
